175a1c0c7bdcf2169105d8ba518ba55af8c9c96e
Al-amin Sheikh <azalamins@gmail.com>
2022-04-13 10:24:59 +0600

    added email password auth

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,32 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Login.css';
+import React, { useRef } from "react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
+import "./Login.css";
 
 const Login = () => {
-    return (
-      <div className="form-container">
-        <div className="form-main">
-          <h1>Please Login</h1>
-          <form>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <input type="email" name="email" id="" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input type="password" name="password" id="" />
-              <small style={{ textAlign: "center" }}>
-                New to Our Site? <Link to="/signup">Create an account</Link>
-              </small>
-            </div>
-            <input className="submit-btn" type="submit" value="Login" />
-          </form>
-          <button className="custom-btn">
-            Sign In With Google
-          </button>
-        </div>
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+  const emailRef = useRef('');
+  const passwordRef = useRef('');
+  const navigate = useNavigate();
+  
+  const handleLogin = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    signInWithEmailAndPassword(email, password);
+  };
+
+  if (user) {
+    console.log(user);
+    navigate('/')
+  }
+
+  return (
+    <div className="form-container">
+      <div className="form-main">
+        <h1>Please Login</h1>
+        <form onSubmit={handleLogin}>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input ref={emailRef} type="email" name="email" id="email" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              ref={passwordRef}
+              type="password"
+              name="password"
+              id="password"
+            />
+            <p style={{color: 'red'}}>{error?.message.includes('wrong') &&'Wrong password'}</p>
+          </div>
+          <input className="submit-btn" type="submit" value="Login" />
+          <small style={{ textAlign: "center" }}>
+            New to Our Site? <Link to="/signup">Create an account</Link>
+          </small>
+        </form>
+        <button className="custom-btn">Sign In With Google</button>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Login;
